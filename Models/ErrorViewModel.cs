82beb358a1b4add4eea01a6cd7d2b042c23b5449
RHPrincy <randimbimananaprincy@gmail.com>
2025-07-22 14:39:00 +0300

    Initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace FosaEnglishClub.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
